34f9ab6ca210c41b486bc9f4a9f34bd068002e8b
Ganesh <ganeshgarje716@gmail.com>
2026-06-21 19:03:51 +0530

    Create StudentNotFoundException class

---
+++ Spring-RestAPI-1/src/main/java/com/ganesh/controller/StudentNotFoundException.java
@@ -0,0 +1,11 @@
+package com.ganesh.controller;
+
+public class StudentNotFoundException extends RuntimeException {
+	
+	public StudentNotFoundException(String msg) {
+		
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+
+}
