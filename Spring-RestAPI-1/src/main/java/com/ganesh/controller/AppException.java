1b54d9b4a1a5235d4c9cb60c962f73b7a8d5ac70
Ganesh <ganeshgarje716@gmail.com>
2026-06-21 19:04:06 +0530

    AppException class

---
+++ Spring-RestAPI-1/src/main/java/com/ganesh/controller/AppException.java
@@ -0,0 +1,17 @@
+package com.ganesh.controller;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class AppException {
+	
+	
+//	@ExceptionHandler(value = StudentNotFoundException.class)
+//	public String handalStudentNotFoundException(Exception e) {
+//		
+//		return e.getMessage();
+//	}
+
+}
